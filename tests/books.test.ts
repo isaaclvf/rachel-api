a8d2156d6bbd51b61d1cc2126db41266120ad49b
Isaac Farias <isaacfarias006@gmail.com>
2024-04-05 20:36:52 -0300

    test: get by id and put

--- tests/books.test.ts
+++ tests/books.test.ts
@@ -1,6 +1,8 @@
+const mongoose = require("mongoose");
 const request = require("supertest");
 const app = require("../src/app");
 const { MongoMemoryServer } = require("mongodb-memory-server");
+const { Book } = require("../src/models/book.model");
 
 let mongoServer;
 
@@ -11,11 +13,11 @@ beforeAll(async () => {
 });
 
 afterAll(async () => {
+  await mongoose.disconnect();
   await mongoServer.stop();
 });
 
 beforeEach(async () => {
-  const { Book } = require("../src/models/book.model");
   await Book.deleteMany({});
 });
 
@@ -43,5 +45,89 @@ describe("POST /books", () => {
 
     expect(response.body.title).toBe(bookData.title);
     expect(response.body.author).toBe(bookData.author);
+  });
+});
+
+describe("GET /books/:id", () => {
+  let bookId;
+
+  beforeEach(async () => {
+    const newBook = new Book({
+      title: "Test Book",
+      author: "Test Author",
+      edition: 1,
+      isbn: "1234567890",
+      status: "available",
+    });
+    const savedBook = await newBook.save();
+    bookId = savedBook._id;
+  });
+
+  afterEach(async () => {
+    await Book.deleteMany({});
+  });
+
+  it("should return a book by id", async () => {
+    const response = await request(app).get(`/books/${bookId}`).expect(200);
+
+    expect(response.body.title).toBe("Test Book");
+    expect(response.body.author).toBe("Test Author");
+    expect(response.body.edition).toBe("1");
+    expect(response.body.isbn).toBe("1234567890");
+    expect(response.body.status).toBe("available");
+  });
+
+  it("should return 404 if book id is not found", async () => {
+    const response = await request(app)
+      .get("/books/4ddd4b97ae4082f647695fae")
+      .expect(404);
+    expect(response.body.error).toBe("book not found");
+  });
+});
+
+describe("PUT /books/:id", () => {
+  let bookId;
+
+  beforeEach(async () => {
+    const newBook = new Book({
+      title: "Test Book",
+      author: "Test Author",
+      edition: 1,
+      isbn: "1234567890",
+      status: "available",
+    });
+    const savedBook = await newBook.save();
+    bookId = savedBook._id;
+  });
+
+  afterEach(async () => {
+    await Book.deleteMany({})
+  });
+
+  it("should update a book by id", async () => {
+    const updatedData = { title: "Updated Title" };
+    const response = await request(app)
+      .put(`/books/${bookId}`)
+      .send(updatedData)
+      .expect(200);
+
+    expect(response.body.title).toBe(updatedData.title);
+  });
+
+  it("should return 404 if book id is not found", async () => {
+    const updatedData = { title: "Updated Title" };
+    const response = await request(app)
+      .put("/books/4ddd4b97ae4082f647695fae")
+      .send(updatedData)
+      .expect(404);
+    expect(response.body.error).toBe("book not found");
+  });
+
+  it("should return 400 if request body is missing required fields", async () => {
+    const response = await request(app)
+      .put(`/books/${bookId}`)
+      .send({})
+      .expect(400);
+    expect(response.body.error).toBe("missing required fields");
   });
 });
