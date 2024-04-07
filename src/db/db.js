6c26afa841a7504aff332484f43187474f1fb2dd
Isaac <isaacfarias006@gmail.com>
2024-04-06 22:42:31 -0300

    remove logs

--- src/db/db.js
+++ src/db/db.js
@@ -5,7 +5,6 @@ const connectionString = process.env.CONNECTION_STRING;
 async function connect() {
   try {
     await mongoose.connect(connectionString);
-    console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error.message);
   }
@@ -14,7 +13,6 @@ async function connect() {
 async function disconnect() {
   try {
     await mongoose.disconnect();
-    console.log("Disconnected from MongoDB");
   } catch (error) {
     console.error("Error disconnecting from MongoDB:", error.message);
   }
