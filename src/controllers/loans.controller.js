856fa5c25c6c756bd3568afd7ec8e18a6b9ee24e
Leonardo <116222412+leopsdev@users.noreply.github.com>
2024-04-07 00:03:11 -0300

    att laon controller e service

--- src/controllers/loans.controller.js
+++ src/controllers/loans.controller.js
@@ -1,7 +1,15 @@
 const { Router } = require("express");
-
+const loansService = require("../services/loans.service");
 const loansRouter = Router();
 
-loansRouter.get("/", (req, res, next) => {});
+loansRouter.get("/", async (req, res, next) => {
+    try {
+        const { page, limit } = req.query;
+        const loans = await loansService.getAllLoans(page, limit);
+        res.status(200).json(loans);
+      } catch (error) {
+        next(error);
+      }
+});
 
 module.exports = loansRouter;
