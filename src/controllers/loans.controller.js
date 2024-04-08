ff79387666a3e870094370152539d1e11e6bdcd9
Antonio Rafael <104793802+devraffa@users.noreply.github.com>
2024-04-08 16:58:14 -0300

    controllerLoans

--- src/controllers/loans.controller.js
+++ src/controllers/loans.controller.js
@@ -12,4 +12,21 @@ loansRouter.get("/", async (req, res, next) => {
       }
 });
 
+loansRouter.post("/", async (req, res, next) => {
+  try {
+    const { userId, bookId, dueDate } = req.body;
+        if (!userId || !bookId || !dueDate) {
+            throw new MissingFieldsError("Preencha todos os campos obrigatórios");
+        }
+        const newLoan = await Loan.create({ userId, bookId, dueDate });
+        return res.status(201).json(newLoan);
+    } catch (error) {
+        console.error('Erro ao criar empréstimo:', error);
+        if (error instanceof MissingFieldsError || error instanceof NotFoundError) {
+            return res.status(400).json({ message: error.message });
+        }
+        return res.status(500).json({ message: 'Ocorreu um erro ao criar o empréstimo' });
+    }
+});
+
 module.exports = loansRouter;
